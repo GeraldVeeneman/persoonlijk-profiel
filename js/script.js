38c5ef2e9abb5fe6de0eb22e6489b9e3eae25c44
Gerald Veeneman <g.veeneman@gmail.com>
2022-12-07 12:18:39 +0100

    Opmaak fieldset elementen

--- js/script.js
+++ js/script.js
@@ -43,13 +43,12 @@ function setSelectTitle(e) {
 	toggleHidden(selectDropdown);
 };
 
-
-var currentTab = 0; // Current tab is set to be the first tab (0)
+let currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
 function showTab(n) {
   // This function will display the specified tab of the form...
-  var x = document.getElementsByClassName("tab");
+  const x = document.getElementsByClassName("tab");
   x[n].style.display = "block";
   //... and fix the Previous/Next buttons:
   if (n == 0) {
@@ -68,7 +67,7 @@ function showTab(n) {
 
 function nextPrev(n) {
   // This function will figure out which tab to display
-  var x = document.getElementsByClassName("tab");
+  const x = document.getElementsByClassName("tab");
   // Exit the function if any field in the current tab is invalid:
   //if (n == 1 && !validateForm()) return false;
   // Hide the current tab:
